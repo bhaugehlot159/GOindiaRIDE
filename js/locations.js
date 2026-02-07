bf5de968c7919c3bacd093fea86fe5eb96b7cc83
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-07 15:38:39 +0530

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -5,3 +5,61 @@ const locations = [
     lat: 23.8431,
     lng: 73.7147
   }
+window.RajasthanData = window.RajasthanData || {};
+
+window.RajasthanData["Dungarpur"] = [
+  {
+    name: "Beneshwar Dham",
+    lat: 23.6411,
+    lng: 73.6056
+  },
+  {
+    name: "Gaib Sagar Lake",
+    lat: 23.8451,
+    lng: 73.7147
+  },
+  {
+    name: "Juna Mahal (Dungarpur Palace)",
+    lat: 23.8459,
+    lng: 73.7155
+  },
+  {
+    name: "Udai Bilas Palace",
+    lat: 23.8472,
+    lng: 73.7163
+  },
+  {
+    name: "Dev Somnath Temple",
+    lat: 23.7685,
+    lng: 73.6028
+  }
+];
+window.RajasthanData = window.RajasthanData || {};
+
+window.RajasthanData["Banswara"] = [
+  {
+    name: "Tripura Sundari Temple (Talwara)",
+    lat: 23.5589,
+    lng: 74.4511
+  },
+  {
+    name: "Mahi Dam",
+    lat: 23.5456,
+    lng: 74.1739
+  },
+  {
+    name: "Kagdi Pick Up Weir",
+    lat: 23.5524,
+    lng: 74.4321
+  },
+  {
+    name: "Anand Sagar Lake",
+    lat: 23.5502,
+    lng: 74.4418
+  },
+  {
+    name: "Cheech Talai",
+    lat: 23.5631,
+    lng: 74.4703
+  }
+];
