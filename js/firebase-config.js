6da5c539ec77ba93601d42589a1da7e9a0a3bdad
Dhaval Gajjar <bhaugehlot159@gemail.com>
2026-03-10 09:09:34 +0530

    chore: add firebase web sdk credentials for otp auth

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,11 @@
 // Firebase Web SDK configuration for GO India RIDE.
 // Project: gehlot-86e38
-// IMPORTANT: apiKey, appId, messagingSenderId ko Firebase Console se copy karke fill karein.
 window.GOINDIARIDE_FIREBASE_CONFIG = {
-    apiKey: '',
+    apiKey: 'AIzaSyDALwUMYGGhDuqKRYDQICF1QwnDsJwalik',
     authDomain: 'gehlot-86e38.firebaseapp.com',
     projectId: 'gehlot-86e38',
-    appId: '',
-    messagingSenderId: '1086303809008'
+    storageBucket: 'gehlot-86e38.firebasestorage.app',
+    messagingSenderId: '1086303809008',
+    appId: '1:1086303809008:web:4325934708c7770c2d4135',
+    measurementId: 'G-LJSEHPM2XH'
 };
