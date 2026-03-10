85b4201d3ec963d94b9ec785d9fb0a510f33d9be
Dhaval Gajjar <bhaugehlot159@gemail.com>
2026-03-10 08:51:19 +0530

    chore: prefill firebase project defaults for otp setup

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,10 @@
-﻿// Fill this file with your Firebase project web config values.
-// You can copy config from Firebase Console > Project settings > General > Your apps > SDK setup and configuration.
+// Firebase Web SDK configuration for GO India RIDE.
+// Project: gehlot-86e38
+// IMPORTANT: apiKey, appId, messagingSenderId ko Firebase Console se copy karke fill karein.
 window.GOINDIARIDE_FIREBASE_CONFIG = {
     apiKey: '',
-    authDomain: '',
-    projectId: '',
+    authDomain: 'gehlot-86e38.firebaseapp.com',
+    projectId: 'gehlot-86e38',
     appId: '',
     messagingSenderId: ''
 };
